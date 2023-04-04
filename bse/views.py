9fff0857f09c0dadf049f065231ea21890f828e9
Sayantan <sonusayantan25@gmail.com>
2023-04-05 03:24:03 +0530

    latest

--- bse/views.py
+++ bse/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
-
+rooms = [
+    {'id': 1, 'name': 'Lets learn Pyhon!'},
+    {'id': 2, 'name': 'Lets learn java'},
+    {'id': 3, 'name': 'Lets learn sql'}
+]
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'bse/home.html', context)
 
-def room(request):
-    return render(request,'room.html')
+def room(request, pk):
+    room= None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request,'bse/room.html', context)
